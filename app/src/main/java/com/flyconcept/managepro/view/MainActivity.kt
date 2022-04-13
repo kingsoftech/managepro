f11e075861d16dc962f3005e80f0b43fac8b8898
Oyero Abdullahi Surajudeen <39423180+kingsoftech@users.noreply.github.com>
2022-04-13 22:24:28 +0100

    ManagePro

--- app/src/main/java/com/flyconcept/managepro/view/MainActivity.kt
+++ app/src/main/java/com/flyconcept/managepro/view/MainActivity.kt
@@ -1,12 +1,86 @@
 package com.flyconcept.managepro.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.bumptech.glide.Glide
 import com.flyconcept.managepro.R
+import com.flyconcept.managepro.databinding.ActivityMainBinding
+import com.flyconcept.managepro.databinding.NavHeaderMainBinding
+import com.flyconcept.managepro.firebase.FirestoreClass
+import com.flyconcept.managepro.model.User
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(),NavigationView.OnNavigationItemSelectedListener{
+    var activityMainBinding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding!!.root)
+        setupActionBar()
+        activityMainBinding!!.navView.setNavigationItemSelectedListener(this)
+        FirestoreClass().signInUser(this)
+    }
+
+    private fun setupActionBar(){
+        val toolbarMainActivity = activityMainBinding!!.appBarMain.toolbarMainActivity
+        setSupportActionBar(toolbarMainActivity)
+        toolbarMainActivity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+        toolbarMainActivity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout = activityMainBinding!!.drawerLayout
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            doubleBackToExit()
+        }
+    }
+    private fun toggleDrawer() {
+        val drawerLayout = activityMainBinding!!.drawerLayout
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_my_profile->{
+                Toast.makeText(this, "my profile", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_sign_out->{
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+        activityMainBinding!!.drawerLayout .closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    fun updateNavigationUserDetails(loggedInUser: User) {
+        val navView =  NavHeaderMainBinding.inflate(layoutInflater)
+        Glide
+            .with(this)
+            .load(loggedInUser)
+            .placeholder(R.drawable.ic_user_place_holder)
+            .into(navView.navUserImage)
+        navView.tvUsername.text = loggedInUser.name
+
     }
 }
